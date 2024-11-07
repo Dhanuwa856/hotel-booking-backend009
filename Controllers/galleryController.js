3cc577146934f757c6dd628ccbb64fbb36f88eb1
Dhanushka Rathnayaka <pdhanushka856@gmail.com>
2024-11-07 22:58:36 +0530

    gallery item update function added

--- Controllers/galleryController.js
+++ Controllers/galleryController.js
@@ -45,3 +45,27 @@ export const deleteGalleryItem = async (req, res) => {
     });
   }
 };
+
+export const updateGalleryItem = async (req, res) => {
+  const galleryId = req.params.id;
+  const updatedData = req.body;
+
+  try {
+    const updateddGalleryItem = await GalleryItem.findByIdAndUpdate(
+      { _id: galleryId },
+      updatedData,
+      { new: true }
+    );
+    if (!updateGalleryItem) {
+      return res.status(404).json({ message: `'${galleryId}' not found` });
+    }
+    res.status(200).json({
+      message: "Category updated successfully",
+      gallery: updateddGalleryItem,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Failed to update Gallery Item", error: err.message });
+  }
+};
--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -3,6 +3,7 @@ import {
   createGalleryItem,
   deleteGalleryItem,
   getGalleryItems,
+  updateGalleryItem,
 } from "../Controllers/galleryController.js";
 import { checkAdmin, checkLoggedIn } from "../Controllers/userController.js";
 
@@ -11,5 +12,6 @@ const galleryRouter = express.Router();
 galleryRouter.get("/", getGalleryItems);
 galleryRouter.post("/", checkLoggedIn, checkAdmin, createGalleryItem);
 galleryRouter.delete("/:id", checkLoggedIn, checkAdmin, deleteGalleryItem);
+galleryRouter.put("/:id", checkLoggedIn, checkAdmin, updateGalleryItem);
 
 export default galleryRouter;
